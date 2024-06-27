a1fac2250f6df3e8700f3e94ae89eb92b592b5b9
IlonaWishec <ilonawishec.management@gmail.com>
2024-06-27 16:29:59 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+total_homeworks = 12
+hours_spent = 1.5
+school = 'Python'
+one_lesson = hours_spent / total_homeworks
+print('Курс:', school + ',','всего задач:', total_homeworks, end='')
+print(',', 'затрачено часов:', hours_spent, end='')
+print(',', 'среднее время выполнения', one_lesson, 'часа.')
