c8f44767eb3cf5fdef6ae1dc3794b7d88bfc032c
IlonaWishec <ilonawishec.management@gmail.com>
2024-10-31 00:39:34 +0300

    homework2.py

--- homework2.py
+++ homework2.py
@@ -5,3 +5,4 @@ one_lesson = hours_spent / total_homeworks
 print('Курс:', school + ',','всего задач:', total_homeworks, end='')
 print(',', 'затрачено часов:', hours_spent, end='')
 print(',', 'среднее время выполнения', one_lesson, 'часа.') #Курс: Python, всего задач: 12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
+print('Hello, Metin')
